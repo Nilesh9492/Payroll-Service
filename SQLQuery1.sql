95d5a49bf01ff645f124b06dc263afc44b1d36de
Nilesh9492 <nilesh94041@gmail.com>
2021-11-07 10:58:30 +0530

    Create payroll Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,12 @@
 --UC1--
 create database paroll_service;
+
+--UC2--
+create table employee_payroll
+(
+EmpID int identity(1,1) primary key,
+name varchar(20) not null,
+salary float,
+startDate date,
+emailID varchar(20)
+);
