ed26c554527186910de50c1659ffa16a6a633098
Nilesh9492 <nilesh94041@gmail.com>
2021-11-07 11:06:05 +0530

    Create Employee Payroll Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,3 +10,7 @@ salary float,
 startDate date,
 emailID varchar(20)
 );
+
+--UC3--
+Insert into employee_payroll(name,salary,startDate,emailId) values ('NILESH',55555,'2021-11-07','nil@gmail.com'),('nil',44440,'2021-07-05','hgf@gmail.com');
+Insert into employee_payroll values ('niles',90000,'2019-05-16','dfghf@gmail.com'),('amamn',2545440,'2020-10-20','fggf@gmail.com');
